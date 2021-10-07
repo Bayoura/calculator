f7bd66303fd0b825766b72e4ad0ca41db82fe7a6
Bayoura <Kristin.Seidelmann@gmx.de>
2021-10-07 15:58:17 +0200

    calculating without equals button works

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const operatorsList_button = document.querySelectorAll('.operator');
 const equals_button = document.querySelector('.equals');
 const allClear_button = document.querySelector('.all-clear');
 const delete_button = document.querySelector('.delete');
-let lastOperand_div = document.querySelector('.top-display');
+let previousOperand_div = document.querySelector('.top-display');
 let currentOperand_div = document.querySelector('.btm-display');
 
+let previousOperand = '';
 let currentOperand = '';
-let lastOperand = '';
-let operator = '';
+let previousOperator = '';
+let currentOperator = '';
 let result = '';
+
+allClear_button.addEventListener('click', () => {
+    currentOperand_div.innerText = '';
+    previousOperand_div.innerText = '';
+});
+
+delete_button.addEventListener('click', () => {
+    currentOperand_div.innerText = currentOperand_div.innerText.slice(0, -1);
+});
 
 numbersList_button.forEach(button => button.addEventListener('click', () => {
     if (button.innerText === '.' && currentOperand_div.innerText.includes('.')) return;
@@ -20,46 +30,54 @@ numbersList_button.forEach(button => button.addEventListener('click', () => {
 }));
 
 function changeSign() {
-    if (currentOperand_div.innerText === '') return;
+    if (currentOperand_div.innerText === '' || currentOperand_div.innerText === '-') return;
     currentOperand_div.innerText *= -1; 
 }
 
-allClear_button.addEventListener('click', () => {
-    currentOperand_div.innerText = '';
-    lastOperand_div.innerText = '';
-});
-
-delete_button.addEventListener('click', () => {
-    currentOperand_div.innerText = currentOperand_div.innerText.slice(0, -1);
-});
-
 operatorsList_button.forEach(button => button.addEventListener('click', () => {
-    determineOperands();
-    if (currentOperand_div.innerText === '') return;
-    if (lastOperand_div.innerText !== '') operate(currentOperand, lastOperand, operator);
+    if (currentOperand_div.innerText === '' || currentOperand_div.innerText === '-') return;
+    if (previousOperand_div.innerText !== '') {
+        // determineOperands();
+        currentOperand = parseFloat(currentOperand_div.innerText);
+        currentOperator = button.innerText;
+        operate(currentOperand, previousOperand, previousOperator);
+        displayInterimResult(result);
+    }
     else {
-        lastOperand_div.innerText += ` ${currentOperand_div.innerText} ${button.innerText}`;
+        previousOperator = button.innerText;
+        previousOperand = parseFloat(currentOperand_div.innerText);
+        previousOperand_div.innerText += ` ${previousOperand} ${previousOperator}`;
         currentOperand_div.innerText = '';
     }
-}))
+}));
 
-function determineOperands() {
-    currentOperand = parseFloat(currentOperand_div.innerText);
-    lastOperand = parseFloat(lastOperand_div.innerText.slice(0, -1));
-    operator = lastOperand_div.innerText.slice(-1);
-}
+// function determineOperands() {
+//     currentOperand = parseFloat(currentOperand_div.innerText);
+//     // previousOperand = parseFloat(previousOperand_div.innerText.slice(0, -1));
+//     // previousOperator = previousOperand_div.innerText.slice(-1);
+// }
+
+let displayInterimResult = (result) => {
+    previousOperand_div.innerText = ` ${result} ${currentOperator}`;
+    currentOperand_div.innerText = '';
+    previousOperator = currentOperator;
+    previousOperand = result;
+};
 
 equals_button.addEventListener('click', () => {
-    determineOperands();
-    operate(currentOperand, lastOperand, operator);
-    lastOperand_div.innerText = `${lastOperand} ${operator} ${currentOperand} =`
+    if (currentOperand_div.innerText === '' || previousOperand_div.innerText === '' || previousOperator === '' || previousOperand_div.innerText.includes('=')) return;
+    // determineOperands();
+    currentOperand = parseFloat(currentOperand_div.innerText);
+    operate(currentOperand, previousOperand, previousOperator);
+    previousOperand_div.innerText = `${previousOperand} ${previousOperator} ${currentOperand} =`
     currentOperand_div.innerText = result;
-})
+    previousOperator = currentOperator;
+    previousOperand = result;
+    currentOperand = '';
+
+});
 
 function operate(current, last, operator) {
-    console.log(last);
-    console.log(current);
-    console.log(operator);
     switch(operator) {
         case '+':
             add(last, current);
@@ -67,7 +85,7 @@ function operate(current, last, operator) {
         case '-':
             substract(last, current);
             break;
-        case 'x':
+        case '×':
             multiply(last, current);
             break;
         case '÷':
@@ -76,27 +94,10 @@ function operate(current, last, operator) {
     }
 }
 
-let displayResult = (result) => lastOperand_div.innerText = result;
-
-let add = (num1, num2) => {
-    result = num1 + num2;
-    displayResult(result);
-};
-
-let substract = (num1, num2) => {
-    result = num1 - num2;
-    displayResult(result);
-};
-
-let multiply = (num1, num2) => {
-    result = num1 * num2;
-    displayResult(result);
-};
-
-let divide = (num1, num2) => {
-    result = num1 / num2;
-    displayResult(result);
-};
+let add = (num1, num2) => result = num1 + num2;
+let substract = (num1, num2) => result = num1 - num2;
+let multiply = (num1, num2) => result = num1 * num2;
+let divide = (num1, num2) => result = num1 / num2;
 
 // window.addEventListener('keydown', function(e){
 //     console.log(e);
